4d7d3275159e6246fcb0113ef50512f4a608d0ff
jyothirmaiLingineni <132278398+jyothirmaiLingineni@users.noreply.github.com>
2023-05-01 20:54:43 +0100

    Update RestassuredAPIExamples.java

--- src/test/java/RestassuredAPIExamples.java
+++ src/test/java/RestassuredAPIExamples.java
@@ -35,9 +35,10 @@ public class RestassuredAPIExamples {
 
         JSONObject request = new JSONObject();
 
-        request.put("firstName", "Tom");
+        request.put("firstName", "Tommy");
         request.put("lastName", "Cooper");
-        request.put("id", '5');
+        request.put("subjectId", "1");
+        request.put("id", '4');
 
         baseURI = "http://localhost:3000/";
         given().
@@ -51,6 +52,36 @@ public class RestassuredAPIExamples {
                 .statusCode(201)
                 .log().all();
     }
+    @Test
+    public void test4_patch() {
 
+        JSONObject request = new JSONObject();
+
+        request.put("lastName", "Casper");
+
+        baseURI = "http://localhost:3000/";
+        given().
+                contentType(ContentType.JSON).
+                accept(ContentType.JSON).
+                header("Content-Type", "application/json").
+                body(request.toJSONString()).
+                when().
+                patch("/users/4").
+                then()
+                .statusCode(200)
+                .log().all();
+    }
+
+    @Test
+    public void test5_delete() {
+
+        baseURI = "http://localhost:3000/";
+
+                when().
+                delete("/users/4").
+                then()
+                .statusCode(200)
+                .log().all();
+    }
 
 }
