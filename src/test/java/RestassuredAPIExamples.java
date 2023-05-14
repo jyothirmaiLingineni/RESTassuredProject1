e613c3d4e2cb2f57d7e1437f835918758fc033b8
jyothirmaiLingineni <132278398+jyothirmaiLingineni@users.noreply.github.com>
2023-05-14 16:42:17 +0100

    Update RestassuredAPIExamples.java

--- src/test/java/RestassuredAPIExamples.java
+++ src/test/java/RestassuredAPIExamples.java
@@ -1,13 +1,14 @@
+import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import org.json.simple.JSONObject;
 import org.testng.annotations.Test;
-import io.restassured.RestAssured;
 
 import static io.restassured.RestAssured.*;
 
 public class RestassuredAPIExamples {
 
-    @Test
+
+    @Test(priority = 1)
     public void test1_get() {
 
         baseURI = "http://localhost:3000";
@@ -16,21 +17,10 @@ public class RestassuredAPIExamples {
                 .then()
                 .statusCode(200)
                 .log().all();
+
     }
 
-    @Test
-    public void test2_get() {
-
-        baseURI = "http://localhost:3000";
-        given().
-                param("name", "Automation")
-                .get("/subjects")
-                .then()
-                .statusCode(200)
-                .log().all();
-    }
-
-    @Test(priority = 1)
+     @Test(priority = 2)
     public void test3_post() {
         //Specify base URI
         RestAssured.baseURI="http://localhost:3000/";
@@ -55,7 +45,7 @@ public class RestassuredAPIExamples {
     }
 
 
-    @Test(priority = 2)
+    @Test(priority = 3)
     public void test4_patch() {
 
         JSONObject request = new JSONObject();
@@ -75,7 +65,7 @@ public class RestassuredAPIExamples {
                 .log().all();
     }
 
-    @Test(priority = 3)
+    @Test(priority = 4)
     public void test5_get() {
 
         baseURI = "http://localhost:3000";
@@ -86,7 +76,7 @@ public class RestassuredAPIExamples {
                 .log().all();
     }
 
-    @Test(priority = 4)
+    @Test(priority = 5)
     public void test5_delete() {
 
         baseURI = "http://localhost:3000/";
@@ -98,15 +88,27 @@ public class RestassuredAPIExamples {
                 .log().all();
     }
 
-    @Test(priority = 5)
-    public void test6_get() {
+//    @Test(priority = 6)
+//    public void test6_get() {
+//
+//        baseURI = "http://localhost:3000";
+//        given().
+//                get("/users")
+//                .then()
+//                .statusCode(204)
+//                .log().all();
+//    }
 
-        baseURI = "http://localhost:3000";
-        given().
-                get("/users")
-                .then()
-                .statusCode(204)
-                .log().all();
-    }
+//    @Test
+//    public void test2_get() {
+//
+//        baseURI = "http://localhost:3000";
+//        given().
+//                param("name", "Automation")
+//                .get("/subjects")
+//                .then()
+//                .statusCode(200)
+//                .log().all();
+//    }
 
 }
